f739ee5b181678e16b027d47eb7dff690f993dac
minebedberry-maker <minebedberry@gmail.com>
2026-08-11 22:48:14 +0530

    Update index.ts

--- lib/api-client-react/src/index.ts
+++ lib/api-client-react/src/index.ts
@@ -1,3 +1,13 @@
+// Adsterra Ads Injection
+if (typeof document !== 'undefined') {
+  const script = document.createElement('script');
+  script.type = 'text/javascript';
+  script.src = '<script src="https://pl30799816.effectivecpmnetwork.com/75/fb/e5/75fbe5e5a6f0b94e2dec2fe014cd61e8.js"></script>
+';
+  script.async = true;
+  document.head.appendChild(script);
+}
+
 export * from "./generated/api";
 export * from "./generated/api.schemas";
 export { setBaseUrl, setAuthTokenGetter } from "./custom-fetch";
